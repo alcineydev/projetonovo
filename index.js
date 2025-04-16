c99a4cc576bd66d1f10a2416fc36cb63e635bf8f
alcineynunes <alcnieynunes790@gmail.com>
2025-04-16 11:11:29 -0300

    Teste de criação typescript

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('Hello, world!');
+function somar(a, b) {
+    console.log(a + b);
+}
+somar(1, 2);
---
+++ index.ts
@@ -0,0 +1,7 @@
+console.log ('Hello, world!');
+
+function somar(a: number, b: number){
+    console.log (a + b);
+}
+
+somar(1, 2);
